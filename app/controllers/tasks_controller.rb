71217d82b545827bae6f918dedfc0ed292edd0c2
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-27 21:11:48 +0200

    Fill in the system tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,51 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit destroy update]
 
-    before_action :set_task, only: [:show, :edit, :destroy, :update]
+  def index
+    @tasks = Task.all
+  end
 
-    def index
-        @tasks = Task.all
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit; end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :new
     end
+  end
 
-    def show
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Task was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def new
-        @task = Task.new
-    end
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task deleted successfully. Ciao cacao'
+  end
 
-    def edit
-    end
+  private
 
-    def create
-        @task = Task.new(task_params)
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-        if @task.save
-          redirect_to tasks_path, notice: "Taskx was successfully created."
-        else
-          render :new
-        end
-    end
-
-    def update
-        if @task.update(task_params)
-            redirect_to tasks_path, notice: "Task was successfully updated."
-          else
-            render :edit
-          end
-    end
-
-    def destroy
-        @task.destroy
-        redirect_to tasks_path, notice: "Task deleted successfully. Ciao cacao"
-    end
-    
-    private
-    
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
-    def task_params
-        params.require(:task).permit(:name)
-    end
+  def task_params
+    params.require(:task).permit(:name)
+  end
 end
--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -1,10 +1,35 @@
 require "application_system_test_case"
 
 class TasksTest < ApplicationSystemTestCase
+  setup do
+    @task = tasks(:first)
+  end
+
   test "visiting the index" do
     visit tasks_url
   
     assert_selector "h1", text: "Tasks"
     assert_equal "Do the bed", tasks(:first).name
   end
+
+  test "Showing a task" do
+    visit tasks_url
+    
+    click_link @task.name
+    assert_selector 'h1', text: @task.name
+  end
+  
+  test 'Updating a task' do
+    visit tasks_url
+    # click_link @task.name
+    click_link 'Edit', match: :first
+    assert_selector 'h1', text: 'Edit task'
+
+
+    fill_in "Name", with: "Updated task"
+    click_on "Update task"
+
+    assert_selector 'h1', text: 'Tasks'
+  end
+
 end
