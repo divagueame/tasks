63e6aa0607b0d3bef26b8f89df155f3ac13ba16e
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-31 18:49:09 +0200

    Add Devise. Add Teams Model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,8 @@ class Task < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  # broadcast_prepend_later_to "tasks", partial: "tasks/task", locals: { task: self }, target: "tasks"
-  after_create_commit -> { broadcast_prepend_later_to 'tasks' }
-  after_update_commit -> { broadcast_replace_later_to 'tasks' }
-  after_destroy_commit -> { broadcast_remove_to 'tasks' }
-  broadcasts_to ->(task) { tasks }, inserts_by: :prepend
+#   after_create_commit -> { broadcast_prepend_later_to 'tasks' }
+#   after_update_commit -> { broadcast_replace_later_to 'tasks' }
+#   after_destroy_commit -> { broadcast_remove_to 'tasks' }
+  broadcasts_to ->(task) { 'tasks' }, inserts_by: :prepend
 end
---
+++ db/migrate/20220531164738_add_team_reference_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTeamReferenceToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :tasks, :team, null: false, foreign_key: true
+  end
+end
