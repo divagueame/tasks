d4808360eaa61e51fae9f5c67fd51c90015afa93
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-31 17:07:37 +0200

    Real time broadcasting changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,11 @@
 class Task < ApplicationRecord
-    validates :name, presence: true
+  validates :name, presence: true
 
-    scope :ordered, -> { order(id: :desc) }
+  scope :ordered, -> { order(id: :desc) }
+
+  # broadcast_prepend_later_to "tasks", partial: "tasks/task", locals: { task: self }, target: "tasks"
+  after_create_commit -> { broadcast_prepend_later_to 'tasks' }
+  after_update_commit -> { broadcast_replace_later_to 'tasks' }
+  after_destroy_commit -> { broadcast_remove_to 'tasks' }
+  broadcasts_to ->(task) { tasks }, inserts_by: :prepend
 end
