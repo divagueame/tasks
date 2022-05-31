d21c5e8de16171ce1711866171269f2f1e0bc5ab
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-31 19:54:07 +0200

    Updating broadcasts

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,6 @@ class Task < ApplicationRecord
 #   after_create_commit -> { broadcast_prepend_later_to 'tasks' }
 #   after_update_commit -> { broadcast_replace_later_to 'tasks' }
 #   after_destroy_commit -> { broadcast_remove_to 'tasks' }
-  broadcasts_to ->(task) { 'tasks' }, inserts_by: :prepend
+  broadcasts_to ->(task) { [task.team, 'tasks'] }, inserts_by: :prepend
   belongs_to :team
 end
