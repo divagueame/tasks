eb1fc2706ff405dbbe799e9c580dd436595efeb6
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-13 19:55:39 +0200

    Refactor and add tests

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,7 @@ class Todo < ApplicationRecord
 
   validates :name, presence: true, uniqueness: {scope: :task_id }
   scope :ordered, -> { order(name: :asc) }
+  def previous_todo
+    task.todos.ordered.where('name < ?', name).last
+  end
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#previous_todo returns the task's previous todo if it exists" do
+    assert_equal todos(:one), todos(:two).previous_todo
+  end
+  
+  test "#previous_todo returns nil if there is no task's previous todo" do
+    assert_nil todos(:one).previous_todo
+  end
 end
