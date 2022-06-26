5db61cd32760aa9ee42ea3b21a9816b4d1cb0cee
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-26 11:52:27 +0200

    Adding details inline with turbo

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,7 +1,7 @@
 class DetailsController < ApplicationController
   before_action :set_task
   before_action :set_todo
-  before_action :set_detail, only: [:edit, :update, :destroy]
+  before_action :set_detail, only: %i[edit update destroy]
 
   def new
     @detail = @todo.details.build
@@ -11,7 +11,10 @@ class DetailsController < ApplicationController
     @detail = @todo.details.build(detail_params)
 
     if @detail.save
-      redirect_to task_path(@task), notice: 'Detail created successfully'
+      respond_to do |format|
+        format.html { redirect_to task_path(@task), notice: 'Detail created successfully' }
+        format.turbo_stream { flash.now[:notice] = 'Detail created successfully' }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,7 +24,7 @@ class DetailsController < ApplicationController
 
   def update
     if @detail.update(detail_params)
-      redirect_to task_path(@todo), notice: 'Detail was successfully updated.' 
+      redirect_to task_path(@todo), notice: 'Detail was successfully updated.'
       # respond_to do |format|
       #   format.html { redirect_to task_path(@task), notice: 'Detail was successfully updated.' }
       #   format.turbo_stream { flash.now[:notice] = 'Detail successfully updated' }
@@ -32,9 +35,10 @@ class DetailsController < ApplicationController
   end
 
   def destroy
-    # @todo.destroy
+    @detail.destroy
+    redirect_to task_path(@task), notice: 'Detail deleted successfully. Ciao cacao'
     # respond_to do |format|
-    #   format.html { redirect_to task_path(@task), notice: 'Detail deleted successfully. Ciao cacao' }
+    # format.html { redirect_to task_path(@task), notice: 'Detail deleted successfully. Ciao cacao' }
     #   format.turbo_stream { flash.now[:notice] = 'Detail deleted successfully. Ciao cacao' }
     # end
   end
