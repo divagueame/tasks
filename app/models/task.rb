d42fd81720e6fd3a67c9629a4e649213fc972c79
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-29 20:47:30 +0200

    Add total time to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,16 @@
 class Task < ApplicationRecord
   has_many :todos, dependent: :destroy
+  has_many :details, through: :todos
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
 
-#   after_create_commit -> { broadcast_prepend_later_to 'tasks' }
-#   after_update_commit -> { broadcast_replace_later_to 'tasks' }
-#   after_destroy_commit -> { broadcast_remove_to 'tasks' }
+  def total_time
+    details.sum(&:time)
+  end
+  #   after_create_commit -> { broadcast_prepend_later_to 'tasks' }
+  #   after_update_commit -> { broadcast_replace_later_to 'tasks' }
+  #   after_destroy_commit -> { broadcast_remove_to 'tasks' }
   broadcasts_to ->(task) { [task.team, 'tasks'] }, inserts_by: :prepend
   belongs_to :team
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#total_time returns the sum of the related details time values' do
+    assert_equal 11, tasks(:one).total_time
+  end
 end
