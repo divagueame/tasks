546b09f0bf27e6efa4251a1ab526d09145ab1ea0
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-03 11:19:32 +0200

    Add link in navbar, added helper for counting time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!
-    
-    def index
-    end
+  skip_before_action :authenticate_user!
+
+  def index
+    p user_signed_in?
+    redirect_to tasks_url if user_signed_in?
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def nested_dom_id(*args)
     args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join('_')
   end
+
+  def duration(seconds)
+    ActiveSupport::Duration.build(60 * seconds).inspect
+  end
 end
