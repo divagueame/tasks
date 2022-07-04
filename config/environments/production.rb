c7e809dfa0b71751f56544d06ea79866b4624e7d
Divagueame <martin.arce.teixeira@gmail.com>
2022-07-04 21:10:05 +0200

    Addind Button to update todo status

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'http://teamtask.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'teamtask.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
